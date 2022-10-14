a480bc666ed6c8d28c9dad342559d25dab586470
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-10-14 14:55:32 -0500

    WIP - Primeras pruebas de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipeApp';
+  
+  nombre: string = 'Rodrigo';
+  valor: number = 1000;
+  obj = {
+    'nombre' : 'Rodrigo'
+  }
+
+
+  mostrarNombre(){
+
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+    
+
+  }
+
 }
