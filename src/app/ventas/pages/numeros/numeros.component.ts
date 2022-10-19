3361ad081dd357d4c04ab974f03b16510bb841a9
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-10-19 12:48:52 -0500

    ADD - pipes de números

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
   styleUrls: ['./numeros.component.css']
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ventasNetas: number = 2567789.5567;
+  porcentaje : number = 0.4856;
 
 }
